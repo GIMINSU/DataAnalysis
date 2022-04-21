f33031944d7990e91ab035759fa7238841ad6f4b
best070 <bestiskim@gmail.com>
2022-04-21 17:53:44 +0900

    slack 메시지 block 으로 보내도록 수정하고 함수 추가

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ import requests
 from public_data_config import apisdata
 
 from slack_message import _post_message
+from slack_sdk_message_post import _post_message_with_slack_sdk
+
 from get_holiday import _get_holiday
 
 # kospi200 회사명 리스트 가져오기
@@ -40,7 +42,8 @@ import json
 class BaseService:
     def __init__(self):
         self.myToken = apisdata["slack"]["token"]
-        self.channel = apisdata["slack"]["channel"]
+        self.channel_name = apisdata["slack"]["channel_name"]
+        self.channel_id = apisdata["slack"]["channel_id"]
 
         # 휴장일 데이터 가져오기
         self.mykey = apisdata["holiday"]["decoding_key"]
@@ -61,11 +64,11 @@ class BaseService:
             self._get_stock_list()
             self._post_signal_to_slack()
         except Exception as e:
-            self._post_message(e)
+            _post_message(BaseService(), text = e)
             pass
     
     def _get_stock_list(self):
-        print("do_get_stock_list")
+        print("Do function : _get_stock_list")
         stock_list = []
         if self.market == "kospi":
             for i in range(1, 21):
@@ -85,17 +88,6 @@ class BaseService:
         elif self.market == "sp500":
             self.market_dict = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0].set_index("Symbol")["Security"].to_dict()
             self.market_stock_dict[self.market] = self.market_dict
-
-    def _post_message(self, text):
-        response = requests.post("https://slack.com/api/chat.postMessage",
-            headers={"Authorization": "Bearer "+self.myToken},
-            data={"channel": self.channel,"text": text}
-        )
-        if response.ok:
-            print("Success. Send message with slack.")
-        else:
-            print("Fail. Send message with slack.")
-            pass
 
     def _make_signal(self, df):
         try:
@@ -148,9 +140,9 @@ class BaseService:
                         df.loc[i, "total_buy_price"] = 0
                         df.loc[i, "buy_price"] = 0
             
-            r_dict_symbol = "'''%s'''"%df.iloc[-1]["Symbol"]
-            name = "'''%s'''"%self.market_dict.get(df.iloc[-1]["Symbol"]).replace("\n", "")
-            date = "'''%s'''"%datetime.strftime(df.iloc[-1]["Date"], "%Y-%m-%d").replace("\n", "")
+            r_dict_symbol = df.iloc[-1]["Symbol"]
+            name = self.market_dict.get(df.iloc[-1]["Symbol"]).replace("\n", "")
+            date = datetime.strftime(df.iloc[-1]["Date"], "%Y-%m-%d").replace("\n", "")
             trade_signal = df.iloc[-1]["trade"]
             price = round(df.iloc[-1]["Close"], 2)
             remain_shares = df.iloc[-1]["shares"]
@@ -167,12 +159,11 @@ class BaseService:
             return r_dict, r_dict_symbol, trade_signal
             
         except Exception as e:
-            # self._post_message(e)
             print("_make_signal Exception", e)
             pass
 
     def _post_signal_to_slack(self):
-        print("Do _post_signal_to_slack function.")
+        print("Do function : _post_signal_to_slack function")
         df = pd.DataFrame()
         buy_list = []
         sell_list = []
@@ -180,7 +171,8 @@ class BaseService:
         sell_idata = []
 
         if self.market == "kospi":
-            for symbol in self.market_stock_dict[self.market]:
+            for symbol in list(self.market_stock_dict[self.market].keys()):
+            # for symbol in list(self.market_stock_dict[self.market].keys())[:20]:  # 테스트 할 때 20개만 테스트
                 ticker = symbol+".KS"
                 try:
                     df = yf.download(ticker, start=self.start_date, auto_adjust=True).reset_index()
@@ -194,7 +186,6 @@ class BaseService:
                         sell_list.append(self.market_dict.get(symbol))
                         sell_idata.append(r_dict)
                 except Exception as e:
-                    # self._post_message(e)
                     print("kospi _post_signal_to_slack Exception", e)
                     pass
 
@@ -214,95 +205,91 @@ class BaseService:
                         sell_idata.append(r_dict)
 
                 except Exception as e:
-                    # self._post_message(e)
                     print("sp500 _post_signal_to_slack Exception", e)
                     pass
 
         # 구매 데이터
         if len(buy_list) == 0:
-            self._post_message("Today not exists buy stocks")
+            _post_message(BaseService(), text = "Today not exists buy stocks")
         if len(buy_list) > 0:
-            self._post_message("Buy candidate stocks : %s"%(buy_list))
-            info_message = {"blocks":buy_idata}
-            print(info_message)
-            self._post_message(info_message)
+            _post_message(BaseService(), text = "Buy candidate stocks : %s"%(buy_list))
+            _post_message_with_slack_sdk(BaseService(), blocks=buy_idata)
 
         # 판매 데이터
         if len(sell_list) == 0:
-            self._post_message("Today not exists sell stocks")
+            _post_message(BaseService(), text = "Today not exists sell stocks")
         if len(sell_list) > 0:
-            self._post_message("Sell candidate stocks : %s"%(sell_list))
-            info_message = {"blocks":sell_idata}
-            print(info_message)
-            self._post_message(info_message)
+            _post_message(BaseService(), text = "Sell candidate stocks : %s"%(sell_list))
+            _post_message_with_slack_sdk(BaseService(), blocks=sell_idata)
         
         # try:
         #     df = pd.DataFrame.from_dict(buy_idata)
         #     df = df.append(pd.DataFrame.from_dict(sell_idata))
         # except Exception as e:
-        #     self._post_message(e)
+        #     _post_message(BaseService(), text = e)
         #     pass
 
         # return df
         
     def _get_priority_house_jungso(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        # url = "https://www.smes.go.kr/sanhakin/websquare/wq_main.do"
-        url = "https://www.mss.go.kr/site/gyeonggi/ex/bbs/List.do?cbIdx=323"
-        driver.get(url)
-        driver.implicitly_wait(10)
-        driver.find_element(By.XPATH, '//*[@id="genTopMenu_2_liTopMenu"]').click()
-        driver.implicitly_wait(1)
-        driver.find_element(By.XPATH, '//*[@id="genLeftMenu_3_leftMenuGrp"]').click()
-        driver.implicitly_wait(1)
 
         idata = []
-        # pagelist1_page_2
         search_date = datetime.today().date()
         str_search_date = datetime.strftime(search_date, "%Y-%m-%d")
-        for page in range(1, 3):
-            # print(page)
-            driver.find_element(By.XPATH, '//*[@id="pagelist1_page_%s"]'%page).click()
-            driver.implicitly_wait(10)
-            for table_order in range (0, 10):
-                title = driver.find_element_by_css_selector('#gridView1_cell_%s_0'%table_order).text
-                # title = driver.find_element_by_css_selector('#contents_inner > div > table > tbody > tr:nth-child(%s) > td.mobile > a > div.subject > strong'%(table_order)).text
-                str_start_date = driver.find_element_by_css_selector('#gridView1_cell_%s_3'%table_order).text.split(' ')[0]
-                str_end_date = driver.find_element_by_css_selector('#gridView1_cell_%s_3'%table_order).text.split(' ')[-1]
-                start_date = datetime.strptime(str_start_date, "%Y-%m-%d").date()
-                end_date = datetime.strptime(str_end_date, "%Y-%m-%d").date()
-                if search_date <= start_date or search_date <= end_date:
-                    idict = {
-                        "search_date" : str_search_date,
-                        "title" : title,
-                        "start_date" : str_start_date,
-                        "end_date" : str_end_date
-                        }
-                    idata.append(idict)
-            last_date = datetime.strptime(driver.find_element_by_css_selector('#gridView1_cell_9_3').text.split(' ')[0], "%Y-%m-%d").date()
-            if search_date > last_date:
-                break
-        driver.quit()
+        try:
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+            url = "https://www.smes.go.kr/sanhakin/websquare/wq_main.do"
+            # url = "https://www.mss.go.kr/site/gyeonggi/ex/bbs/List.do?cbIdx=323"
+            driver.get(url)
+            driver.implicitly_wait(20)
+            driver.find_element(By.XPATH, '//*[@id="genTopMenu_2_liTopMenu"]').click()
+            driver.implicitly_wait(20)
+            driver.find_element(By.XPATH, '//*[@id="genLeftMenu_3_leftMenuGrp"]').click()
+            driver.implicitly_wait(20)
+
+            for page in range(1, 3):
+                # print(page)
+                driver.find_element(By.XPATH, '//*[@id="pagelist1_page_%s"]'%page).click()
+                driver.implicitly_wait(20)
+                for table_order in range (0, 10):
+                    title = driver.find_element_by_css_selector('#gridView1_cell_%s_0'%table_order).text
+                    # title = driver.find_element_by_css_selector('#contents_inner > div > table > tbody > tr:nth-child(%s) > td.mobile > a > div.subject > strong'%(table_order)).text
+                    str_start_date = driver.find_element_by_css_selector('#gridView1_cell_%s_3'%table_order).text.split(' ')[0]
+                    str_end_date = driver.find_element_by_css_selector('#gridView1_cell_%s_3'%table_order).text.split(' ')[-1]
+                    start_date = datetime.strptime(str_start_date, "%Y-%m-%d").date()
+                    end_date = datetime.strptime(str_end_date, "%Y-%m-%d").date()
+                    if search_date <= start_date or search_date <= end_date:
+                        idict = {
+                                "type" : "section",
+                                "text" : {
+                                    "type" : "mrkdwn",
+                                    "text": "*search_date* : %s\n*title* : %s\n*start_date* : %s\n*end_date* : %s"%(str_search_date, title, str_start_date, str_end_date)
+                                }
+                            }
+                        idata.append(idict)
+                last_date = datetime.strptime(driver.find_element_by_css_selector('#gridView1_cell_9_3').text.split(' ')[0], "%Y-%m-%d").date()
+                if search_date > last_date:
+                    break
+            driver.quit()
+
+        except Exception as e:
+            _post_message(BaseService(), text = e)
+            pass
 
         if len(idata) > 0:
-            text = str(idata)
-            _post_message(self, text)
+            _post_message_with_slack_sdk(BaseService(), blocks=idata)
         else:
             text = "유효한 새로운 중소기업 장기근속자 주택 특별공급 없음"
-            _post_message(self, text)
+            _post_message(BaseService(), text = text)
         return idata
 
     def worker(self):
-        schedule.every().days.at("09:05").do(self.work)
-        schedule.every().days.at("15:00").do(self.work)
         schedule.every().days.at("05:30").do(self.work)
+        schedule.every().days.at("09:05").do(self.work)
+        schedule.every().days.at("09:30").do(self._get_priority_house_jungso)
+        schedule.every().days.at("15:00").do(self.work)
         schedule.every().days.at("22:35").do(self.work)
-        try:
-            schedule.every().days.at("09:30").do(self._get_priority_house_jungso)
-            schedule.every().days.at("22:40").do(self._get_priority_house_jungso)
-        except Exception as e:
-            print(e)
-            pass
+        schedule.every().days.at("22:40").do(self._get_priority_house_jungso)
 
         while True:
             now = datetime.now().time()
@@ -315,7 +302,7 @@ class BaseService:
                 future = datetime.combine((datetime.today() + timedelta(days=2)), datetime.strptime("00:00", "%H:%M").time())
                 text = "Today is saturday. Next_run_time : %s"%(future)
                 print(text)
-                _post_message(self, text)
+                _post_message(BaseService(), text = text)
                 ot.sleep((future - now).total_seconds())
 
             # 일요일인 경우 하루 뒤 다시 시작
@@ -323,7 +310,7 @@ class BaseService:
                 future = datetime.combine((datetime.today() + timedelta(days=1)), datetime.strptime("00:00", "%H:%M").time())
                 text = "Today is sunday. Next_run_time : %s"%(future)
                 print(text)
-                _post_message(self, text)
+                _post_message(BaseService(), text = text)
                 ot.sleep((future - now).total_seconds())
 
             # 평일이지만 공휴일인 경우 
@@ -331,69 +318,77 @@ class BaseService:
                 future = datetime.combine((datetime.today()), datetime.strptime("22:30", "%H:%M").time())
                 text = "Today is a Korean holiday. Next_run_time : %s"%(future)
                 print(text)
-                _post_message(self, text)
+                _post_message(BaseService(), text = text)
                 ot.sleep((future - now).total_seconds())
 
             if weekno < 5 and str_date not in self.ko_holiday_list: 
                 if now.hour >= 0 and now.hour < 5:
-                    until_time = datetime.combine(datetime.today(), datetime.strptime("05:00", "%H:%M").time())
+                    until_time = datetime.combine(datetime.today(), datetime.strptime("05:28", "%H:%M").time())
                     text = "Pause %s"%(until_time)
                     print(text)
-                    _post_message(self, text)
-                    pause.until(until_time)
-
-                if now.hour >= 6 and now.hour < 9:
-                    until_time = datetime.combine(datetime.today(), datetime.strptime("09:00", "%H:%M").time())
-                    text = "Pause until %s"%(until_time)
-                    print(text)
-                    _post_message(self, text)
-                    pause.until(until_time)
-
-                if now.hour >= 10 and now.hour < 15:
-                    until_time = datetime.combine(datetime.today(), datetime.strptime("14:59", "%H:%M").time())
-                    text = "Pause until %s"%(until_time)
-                    print(text)
-                    _post_message(self, text)
-                    pause.until(until_time)
-
-                if now.hour >= 16 and now.hour < 22:
-                    until_time = datetime.combine(datetime.today(), datetime.strptime("22:32", "%H:%M").time())
-                    text = "Pause until %s"%(until_time)
-                    print(text)
-                    _post_message(self, text)
-                    pause.until(until_time)
-
-                if now.hour >= 23:
-                    until_time = datetime.combine(datetime.today(), datetime.strptime("24:00", "%H:%M").time())
-                    text = "Pause until %s"%(until_time)
-                    print(text)
-                    _post_message(self, text)
+                    _post_message(BaseService(), text = text)
                     pause.until(until_time)
 
                 if (now > time(hour=5, minute=29)) and (now < time(hour=5, minute=31)):
                     text = "Analyzing the SP500. Time. %s"%now
                     print(text)
-                    _post_message(self, text)
+                    _post_message(BaseService(), text = text)
                     self.market = "sp500"
+
+                if now.hour >= 6 and now.hour < 9:
+                    until_time = datetime.combine(datetime.today(), datetime.strptime("09:00", "%H:%M").time())
+                    text = "Pause until %s"%(until_time)
+                    print(text)
+                    _post_message(BaseService(), text = text)
+                    pause.until(until_time)
 
                 if (now > time(hour=9, minute=4)) and (now < time(hour=9, minute=6)):
                     text = "Analyzing the KOSPI. Time. %s"%now
                     print(text)
-                    _post_message(self, text)
+                    _post_message(BaseService(), text = text)
                     self.market = "kospi"
+
+                if now.hour >= 10 and now.hour < 15:
+                    until_time = datetime.combine(datetime.today(), datetime.strptime("14:59", "%H:%M").time())
+                    text = "Pause until %s"%(until_time)
+                    print(text)
+                    _post_message(BaseService(), text = text)
+                    pause.until(until_time)
 
                 if (now > time(hour=14, minute=59)) and (now < time(hour=15, minute=1)):
                     text = "Analyzing the KOSPI. Time. %s"%now
                     print(text)
-                    _post_message(self, text)
+                    _post_message(BaseService(), text = text)
                     self.market = "kospi"
+
+                if now.hour >= 16 and now.hour < 22:
+                    until_time = datetime.combine(datetime.today(), datetime.strptime("22:32", "%H:%M").time())
+                    text = "Pause until %s"%(until_time)
+                    print(text)
+                    _post_message(BaseService(), text = text)
+                    pause.until(until_time)
+
 
                 if (now > time(hour=22, minute=34)) and (now < time(hour=22, minute=36)):
                     text = "Analyzing the SP500. Time. %s"%now
                     print(text)
-                    _post_message(self, text)
+                    _post_message(BaseService(), text = text)
                     self.market = "sp500"
+
+                if now.hour >= 23:
+                    until_time = datetime.combine(datetime.today(), datetime.strptime("24:00", "%H:%M").time())
+                    text = "Pause until %s"%(until_time)
+                    print(text)
+                    _post_message(BaseService(), text = text)
+                    pause.until(until_time)
+
                 
+
+                
+
+
+
+
                 schedule.run_pending()    
                 ot.sleep(59)
 
